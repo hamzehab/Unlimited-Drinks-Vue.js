330ecea2a48b86139b13e7ea3be306d30ff6b7d3
Hamzeh Abdallah <hamzehhabdallah@gmail.com>
2023-12-15 18:20:05 -0500

    deploy SPA

---
+++ assets/auth0.d21e3c0c.js
@@ -0,0 +1,1 @@
+import{d as t}from"./index.37379c88.js";var d=({app:i})=>{i.use(t({domain:"dev-qp4jx4njpfk4xc45.us.auth0.com",clientId:"4BH6oYdvFf4uqZ0s3iEkdDJGZeiCOzFM",authorizationParams:{redirect_uri:"https://hamzehab.github.io/unlimiteddrinks-frontend"}}))};export{d as default};
