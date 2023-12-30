e6c158e09a45a530b1ed1783245ad8ddb9816ab0
Hamzeh Abdallah <hamzehhabdallah@gmail.com>
2023-12-30 15:39:51 -0500

    update path to use new domain name

--- assets/auth0.5d5bf76d.js
+++
@@ -1,1 +0,0 @@
-import{d as t}from"./index.c1592a70.js";var d=({app:i})=>{i.use(t({domain:"dev-qp4jx4njpfk4xc45.us.auth0.com",clientId:"4BH6oYdvFf4uqZ0s3iEkdDJGZeiCOzFM",authorizationParams:{redirect_uri:"https://hamzehab.github.io/unlimiteddrinks-frontend"}}))};export{d as default};
