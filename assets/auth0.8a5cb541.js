44850332e51b6e987a19185cec4e8e07c8175ebd
Hamzeh Abdallah <hamzehhabdallah@gmail.com>
2023-12-30 15:42:53 -0500

    fix: auth0 redirect params set to proper domain
    Previously set to github pages, and is now set to new domain "unlimiteddrinks.net"

--- assets/auth0.8a5cb541.js
+++ assets/auth0.8a5cb541.js
@@ -1,1 +1,11 @@
-import{d as t}from"./index.365d9f13.js";var d=({app:i})=>{i.use(t({domain:"dev-qp4jx4njpfk4xc45.us.auth0.com",clientId:"4BH6oYdvFf4uqZ0s3iEkdDJGZeiCOzFM",authorizationParams:{redirect_uri:"https://hamzehab.github.io/unlimiteddrinks-frontend"}}))};export{d as default};
+import { d as t } from "./index.365d9f13.js";
+var d = ({ app: i }) => {
+  i.use(
+    t({
+      domain: "dev-qp4jx4njpfk4xc45.us.auth0.com",
+      clientId: "4BH6oYdvFf4uqZ0s3iEkdDJGZeiCOzFM",
+      authorizationParams: { redirect_uri: "https://unlimiteddrinks.net" },
+    })
+  );
+};
+export { d as default };
