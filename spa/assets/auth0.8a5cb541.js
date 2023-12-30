deff48c8b18a163dc9ac5cde0e99bc8dc76a7518
Hamzeh Abdallah <hamzehhabdallah@gmail.com>
2023-12-30 15:38:05 -0500

    Revert "update to use new domain"
    This reverts commit 64a666682ff3c5a01576a15353fe3b473f34dde1.

--- spa/assets/auth0.8a5cb541.js
+++
@@ -1,1 +0,0 @@
-import{d as t}from"./index.365d9f13.js";var d=({app:i})=>{i.use(t({domain:"dev-qp4jx4njpfk4xc45.us.auth0.com",clientId:"4BH6oYdvFf4uqZ0s3iEkdDJGZeiCOzFM",authorizationParams:{redirect_uri:"https://hamzehab.github.io/unlimiteddrinks-frontend"}}))};export{d as default};
